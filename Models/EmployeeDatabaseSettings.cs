f613985f02858963879d6b99019efab59210b237
RAJENDER SINGH NEGI <RAJENDER SINGH NEGI@DESKTOP-M9KU0VI>
2022-08-03 13:04:26 +0530

    Add project files.

---
+++ Models/EmployeeDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApi.Models
+{
+    public class EmployeeDatabaseSettings: IEmployeeDatabaseSettings
+    {
+        public string EmployeeCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface IEmployeeDatabaseSettings
+    {
+        public string EmployeeCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,48 @@
+﻿using DemoApi.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApi.Services
+{
+    public class EmployeeService
+    {
+        private readonly IMongoCollection<Employee> _employees;
+        public EmployeeService(IEmployeeDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _employees = database.GetCollection<Employee>(settings.EmployeeCollectionName);
+
+        }
+
+        public List<Employee> Get()
+        {
+            List<Employee> employees;
+            employees = _employees.Find(emp => true).ToList();
+            return employees;
+        }
+
+        public Employee Get(string id) =>
+            _employees.Find<Employee>(emp => emp.Id == id).FirstOrDefault();
+
+        public Employee Create(Employee employee)
+        {
+            _employees.InsertOne(employee);
+            return employee;
+        }
+
+        public void Update(string id, Employee employeeIn) =>
+            _employees.ReplaceOne(book => book.Id == id, employeeIn);
+
+        public void Remove(Employee employeeIn) =>
+            _employees.DeleteOne(book => book.Id == employeeIn.Id);
+
+        public void Remove(string id) =>
+            _employees.DeleteOne(employee => employee.Id == id);
+
+    }
+}
